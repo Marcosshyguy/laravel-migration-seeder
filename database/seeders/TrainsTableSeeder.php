5787f3cda6303d1e98f0d27eff3434129cff709f
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2022-12-20 18:07:11 +0100

    cretea 10 records with for cicle

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -19,17 +19,20 @@ class TrainsTableSeeder extends Seeder
     public function run(Faker $faker)
     {
         $companies_name = ['Trenitalia', 'Italo', 'Trenoni'];
-        $newTrain = new Train();
-        $newTrain->company_name = $faker->randomElement($companies_name);
-        $newTrain->starting_station = $faker->city();
-        $newTrain->arriving_station = $faker->city();
-        $newTrain->starting_hour = $faker->time();
-        $newTrain->arriving_hour = $faker->time();
-        $newTrain->train_date = $faker->dateTimeBetween('-2 week', '+2 week');
-        $newTrain->train_code = $faker->regexify('[A-Z]{4}[0-9]{3}[A-Z]{2}');
-        $newTrain->train_vagon = $faker->numberBetween(1, 20);
-        $newTrain->on_time = $faker->boolean();
-        $newTrain->canceled = $faker->boolean();
-        $newTrain->save();
+        for ($i = 0; $i < 10; $i++) {
+
+            $newTrain = new Train();
+            $newTrain->company_name = $faker->randomElement($companies_name);
+            $newTrain->starting_station = $faker->city();
+            $newTrain->arriving_station = $faker->city();
+            $newTrain->starting_hour = $faker->time();
+            $newTrain->arriving_hour = $faker->time();
+            $newTrain->train_date = $faker->dateTimeBetween('-2 week', '+2 week');
+            $newTrain->train_code = $faker->regexify('[A-Z]{4}[0-9]{3}[A-Z]{2}');
+            $newTrain->train_vagon = $faker->numberBetween(1, 20);
+            $newTrain->on_time = $faker->boolean();
+            $newTrain->canceled = $faker->boolean();
+            $newTrain->save();
+        }
     }
 }
